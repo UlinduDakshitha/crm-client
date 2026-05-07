5937db393aa9758308ff02bb1bab7df07539e8b3
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-07 06:42:13 +0530

    create login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/auth/login", form);
+
+      localStorage.setItem("token", res.data.token);
+
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-8 rounded-2xl shadow-lg w-96"
+      >
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          CRM Login
+        </h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full border p-3 rounded-lg mb-4"
+          onChange={(e) =>
+            setForm({ ...form, email: e.target.value })
+          }
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full border p-3 rounded-lg mb-4"
+          onChange={(e) =>
+            setForm({ ...form, password: e.target.value })
+          }
+        />
+
+        <button className="bg-black text-white w-full py-3 rounded-lg hover:bg-gray-800">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
